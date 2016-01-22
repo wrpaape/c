aeb424c1fb864281e457f5816c1d7ec1c0dfa895
Reid Paape <wrpaape@gmail.com>
2016-01-22 12:45:27 -0600

    stupid sexy pointers

--- strings/strings.c
+++ strings/strings.c
@@ -13,17 +13,17 @@ void report_results(char *final_ptr, int num_replaced, clock_t time_elapsed); /*
 
 int main(void)
 {
-  clock_t start;       /* time of program start */
-  clock_t finish;      /* time of program finish */
-  char input[80];      /* input to be processed */
-  char *string_ptr;    /* pointer to string */
-  char *find_ptr;      /* pointer to target substring */
-  char *replace_ptr;   /* pointer to replacement substring */
-  char *replace_array; /* pointer to array result of 'find_and_replace' */
-
-  start = clock();                                                   /* start time */
+  clock_t start;     /* time of program start */
+  clock_t finish;    /* time of program finish */
+  char input[80];    /* input to be processed */
+  char *string_ptr;  /* pointer to string */
+  char *find_ptr;    /* pointer to target substring */
+  char *replace_ptr; /* pointer to replacement substring */
+  char *results_ptr; /* pointer to array of results from 'find_and_replace' */
 
   string_ptr  = gets_trimmed(input, sizeof(input));                  /* point 'string_ptr' at beginning of input */
+
+  start       = clock();                                             /* start time */
 
   find_ptr    = split_slash(string_ptr);                             /* trim string and set pointer to start of find_str */
 
@@ -31,30 +31,26 @@ int main(void)
 
   results_ptr = find_and_replace(string_ptr, find_ptr, replace_ptr); /* perform global find and replace */
 
-  finish = clock();                                                  /* finish time */
+  finish      = clock();                                             /* finish time */
 
-  report_results(results_ptr[0], results_ptr[1], finish - start);    /* print program results */
-
-  /* printf("string_ptr:  %s\n",  string_ptr); */
-  /* printf("find_ptr:    %s\n",    find_ptr); */
-  /* printf("replace_ptr: %s\n", replace_ptr); */
+  /* report_results(*results_ptr, *(results_ptr + 1), finish - start);    /1* print program results *1/ */
 
   return 0;
 } 
 
-/************************************
- *         - gets_trimmed -         *
- *                                  *
- * retrieves user input from stdin  *
- * and removes the trailing newline *
- ************************************/
+
+/********************************************************************
+ *                         - gets_trimmed -                         *
+ *                                                                  *
+ * retrieves user input from stdin and removes the trailing newline *
+ ********************************************************************/
 char *gets_trimmed(char *input_buffer, int buffer_size)
 {
-  printf("%s\n  \e[5m>\e[25m ", INPUT_FORMAT); /* prompt user for input */
+  printf("%s\n  \e[5m>\e[25m ", INPUT_FORMAT);   /* prompt user for input */
 
-  fgets(input_buffer, buffer_size, stdin);     /* set user input to 'input_buffer' */
+  fgets(input_buffer, buffer_size, stdin);       /* set user input to 'input_buffer' */
 
-  input_buffer[strlen(input_buffer) - 1] = '\0';      /* trim trailing \n */
+  input_buffer[strlen(input_buffer) - 1] = '\0'; /* trim trailing \n */
 
   return input_buffer;
 }
@@ -101,31 +97,64 @@ char *split_slash(char *string_ptr)
  ****************************************************************************/
 char *find_and_replace(char *string_ptr, char *find_ptr, char *replace_ptr)
 {
-  char results_ptr;                          /* pointer to array containing new string and number of replacements made */
-  int length_string   = strlen(*string_ptr); /* length of total string */
-  int length_find_str = strlen(*find_ptr);   /* length of target substring */
+  char results_ptrs[2];             /* pointer to array containing new string and number of replacements made */
+  int length_find_str    = strlen(find_ptr);    /* length of target substring */
+  int length_replace_str = strlen(replace_ptr); /* length of replacement string */
 
-  int string_index = 0;
-  int match_index  = 0;
+  char *final_string_ptr = string_ptr;
+  char *temp_ptr;
+  int match_count   = 0; /* counter tracking the consecutive matches of target substring */
+  int *replace_count_ptr = 0; /* counter tracking the number of replacements made */
 
-  /* while ((strlen(*string_ptr) - string_index) >= strlen(*find_ptr)) { */
+  while (*string_ptr != '\0') {
+    if (*string_ptr == *find_ptr) {
+      ++match_count;
 
+      if (match_count == length_find_str) {
+        temp_ptr = string_ptr;
 
-  /* } */
+        string_ptr -= length_find_str;
 
-  return results_ptr;
+        *string_ptr = *replace_ptr;
+        
+        string_ptr += (length_replace_str + 1);
+
+        string_ptr = temp_ptr;
+
+        ++(*replace_count_ptr);
+
+        find_ptr -= length_find_str;
+      }
+    } else {
+      find_ptr -= match_count;
+
+      match_count = 0;
+    }
+
+    ++string_ptr;
+  }
+
+  printf("final_string_ptr: %s\n", final_string_ptr);
+  printf("replace_count:    %d\n",    replace_count);
+
+  results_ptrs[0] = final_string_ptr;
+  results_ptrs[1] = replace_count_ptr;
+
+  return results_ptrs;
 }
 
 
-/********************************************************************************
- *                              - report_results -                              *
- *                                                                              *
- * prints results of running main program:                                      *
- * 'final_ptr' points to new string with replacements,                          *
- * 'num_replaced' indicates the number of replacement operations performed, and *
- * 'time_elapsed' indicates the CPU time elapsed across all operations          *
- ********************************************************************************/
+/**********************************************************************************
+ *                               - report_results -                               *
+ *                                                                                *
+ * prints results of running main program:                                        *
+ *   'final_ptr' points to new string with replacements,                          *
+ *   'num_replaced' indicates the number of replacement operations performed, and *
+ *   'time_elapsed' indicates the CPU time elapsed across operations on input     *
+ **********************************************************************************/
 void report_results(char *final_ptr, int num_replaced, clock_t time_elapsed)
 {
-
+  /* printf("string_ptr:  %s\n",  string_ptr); */
+  /* printf("find_ptr:    %s\n",    find_ptr); */
+  /* printf("replace_ptr: %s\n", replace_ptr); */
 }
