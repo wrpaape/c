e1a9238f8b1dc614dc4362c2e510483ce08739ba
Reid Paape <wrpaape@gmail.com>
2016-01-23 23:05:09 -0600

    better

--- files/b2a.c
+++ files/b2a.c
@@ -31,24 +31,22 @@
 #define O_BINARY 0                  /* define the flag if not defined yet */
 #endif
 
-#define BUFFER_SIZE (144 * 1024)    /* use 144K buffers */
+#define BUFFER_SIZE (16 * 1024)    /* use 16K buffers */
 
-char next_ascii_char(char **binary_buffer_ptr_ptr); /* performs b2a reversion */
+/* char next_ascii_char(char **binary_buffer_ptr); /1* performs b2a reversion *1/ */
 
 
 int main(int argc, char *argv[])
 {
-  char *binary_buffer_ptr;            /* pointer to buffer for binary data */
-  /* char binary_buffer[BUFFER_SIZE];            /1* pointer to buffer for binary data *1/ */
-	char ascii_buffer[BUFFER_SIZE / 9]; /* buffer for ASCII data */
-  int offset = 0;                     /* offset from start of 'ascii_buffer' */
-	int file;		    	    		    	    /* file descriptor */
-	int read_size;  				    	      /* number of bytes on last read */
+  char binary_buffer[BUFFER_SIZE]; /* buffer for binary data */
+	char ascii_buffer[BUFFER_SIZE];  /* buffer for ASCII data */
+  char ascii_char;                 /* conversion result */
+  int binary_offset;               /* offset from start of 'binary_buffer' */
+  int ascii_offset;                /* offset from start of 'ascii_buffer' */
+  int exp;                         /* exponentiation according to bit position */
+	int file;		    	    		    	 /* file descriptor */
+	int read_size;  				    	   /* number of bytes on last read */
 
-  binary_buffer_ptr = malloc(BUFFER_SIZE);
-
-  /* printf("BUFFER_SIZE: %i\n", BUFFER_SIZE); */
-  /* printf("sizeof(*binary_buffer_ptr): %lu\n", BUFFER_SIZE); */
 
 	if (argc != 2) {
     fprintf(stderr, "\e[31mERROR: wrong number of arguments\n");
@@ -63,18 +61,19 @@ int main(int argc, char *argv[])
     exit(8);
   }
 
+  ascii_offset = 0;
+
   while (1) {
-    /* read_size = read(file, binary_buffer_ptr, BUFFER_SIZE); */
-    read_size = read(file, binary_buffer_ptr, BUFFER_SIZE);
+    read_size = read(file, binary_buffer, sizeof(binary_buffer));
 
     if (read_size == 0) {
       printf("writing file:\n\n");
 
-      ascii_buffer[offset] = '\0'; /* terminate 'ascii_buffer' */
+      ascii_buffer[ascii_offset] = '\0'; /* terminate 'ascii_buffer' */
 
       printf("%s", ascii_buffer);
 
-      write(file, ascii_buffer, offset + 1);
+      write(file, ascii_buffer, ascii_offset + 1);
 
       break; /* end of file */
     }
@@ -84,28 +83,34 @@ int main(int argc, char *argv[])
       exit(8);
     }
 
-    while (*binary_buffer_ptr != '\0') {
-      printf("*binary_buffer_ptr: %c\n", *binary_buffer_ptr);
-      printf("offset: %i\n", offset);
-      printf("sizeof(ascii_buffer): %lu\n", sizeof(ascii_buffer));
+    binary_offset = 0;
 
-      ++binary_buffer_ptr; /* skip leading "bit" */
+    while (binary_buffer[binary_offset] != '\0') {
+      ++binary_offset; /* skip leading "bit" */
 
-      ascii_buffer[offset] = next_ascii_char(&binary_buffer_ptr);
+      ascii_char = 0;
 
-      printf("should be space *binary_buffer_ptr: %c\n", *binary_buffer_ptr);
+      for (exp = 6; exp >= 0; --exp) {
+        if (binary_buffer[binary_offset] == '1') {
+          ascii_char += (1 << exp);
+        }
+        
+        ++binary_offset;
+      }
 
-      ++binary_buffer_ptr; /* skip space */
+      ++binary_offset; /* skip space */
 
       /* printf("*binary_buffer_ptr: %hhi\n", *binary_buffer_ptr); */
 
       /* ++binary_buffer_ptr; /1* skip space *1/ */
       /* printf("*binary_buffer_ptr: %hhi\n", *binary_buffer_ptr); */
       /* ++binary_buffer_ptr; /1* skip space *1/ */
-      printf("*binary_buffer_ptr: %hhi\n", *binary_buffer_ptr);
-      close(file);
-      return 0;
-      ++offset; /* skip "binary" chunk and following space */
+      /* printf("*binary_buffer_ptr: %hhi\n", *binary_buffer_ptr); */
+      /* close(file); */
+      /* return 0; */
+
+      ascii_buffer[ascii_offset] = ascii_char;
+      ++ascii_offset;
     }
   }
 
@@ -121,20 +126,17 @@ int main(int argc, char *argv[])
  * Reads in next 7 "bits" of reerenced by 'binary_buffer_ptr' *
  * and returns the corresponding ASCII char                  *
  *************************************************************/
-char next_ascii_char(char **binary_buffer_ptr_ptr)
-{
-  char ascii_char = 0; /* conversion result */
+/* char next_ascii_char(char *binary_buffer_ptr) */
+/* { */
+/*   char ascii_char = 0; /1* conversion result *1/ */
 
-  for (int exp = 6; exp >= 0; --exp) {
-      printf("    **binary_buffer_ptr_ptr: %c\n", **binary_buffer_ptr_ptr);
-    if (**binary_buffer_ptr_ptr == '1') {
-      ascii_char += (1 << exp);
-    }
+/*   for (int exp = 6; exp >= 0; --exp) { */
+/*     if (*binary_buffer_ptr == '1') { */
+/*       ascii_char += (1 << exp); */
+/*     } */
     
-    ++(*binary_buffer_ptr_ptr);
-  }
-      printf("ascii_char: %c\n", ascii_char);
-      printf("    **binary_buffer_ptr_ptr: %c\n", **binary_buffer_ptr_ptr);
+/*     ++binary_buffer_ptr; */
+/*   } */
 
-  return ascii_char;
-}
+/*   return ascii_char; */
+/* } */
