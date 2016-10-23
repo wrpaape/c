cc20672075950adda70c4d1e9ae7743bf8d6ce80
Reid Paape <wrpaape@gmail.com>
2016-10-23 15:27:08 -0500

    lmao needed to read on stdin instead of argv

--- programming_challenges/jolly_jumpers/jolly_jumpers.c
+++ programming_challenges/jolly_jumpers/jolly_jumpers.c
@@ -1,141 +1,75 @@
-#include <unistd.h>	/* write */
 #include <stdbool.h>	/* bool */
 #include <stdlib.h>	/* strtol */
+#include <stdio.h>	/* getline */
 
-/* '0'..'9' → 0..9 */
-#define ASCII_TO_DIGIT(ASCII) ((ASCII) & 15u)
-
-#define WRITE_OUTPUT_LINE(OUTPUT)					\
-(void) write(STDOUT_FILENO,						\
-	     OUTPUT "\n",						\
-	     sizeof(OUTPUT))
+#define READ_INTEGER(STRING)						\
+(int) strtol(STRING,							\
+	     &STRING,							\
+	     10)
 
 static inline bool
-string_to_integer(int *const restrict integer,
-		  const char *restrict string)
-__attribute__((always_inline));
-
-static inline bool
-string_to_integer(int *const restrict integer,
-		  const char *restrict string)
-{
-	strtol
-	/* int val0; */
-	/* int val1; */
-	/* int val2; */
-	/* int val3; */
-	/* int token; */
-
-	/* token = (int) *string; */
-
-	/* if (token == '-') { */
-	/* 	++string; */
-	/* 	token = (int) *string; */
-	/* 	val0  = -1; */
-	/* } else { */
-	/* 	val0 = 1; */
-	/* } */
-
-	/* val0 *= ASCII_TO_DIGIT(token); */
-
-	/* ++string; */
-	/* token = (int) *string; */
-
-	/* if (token == 0) { */
-	/* 	*integer = val0; */
-	/* 	return true; */
-	/* } */
-
-	/* val1 = ASCII_TO_DIGIT(token); */
-
-	/* ++string; */
-	/* token = (int) *string; */
-
-	/* if (token == 0) { */
-	/* 	*integer = (val0 * 10) + val1; */
-	/* 	return true; */
-	/* } */
-
-	/* val2 = ASCII_TO_DIGIT(token); */
-
-	/* ++string; */
-	/* token = (int) *string; */
-
-	/* if (token == 0) { */
-	/* 	*integer = (val0 * 100) + (val1 * 10) + val2; */
-	/* 	return true; */
-	/* } */
-
-	/* const bool success = ((val0 < 3) && (string[1] == '\0')); */
-
-	/* if (success) { */
-	/* 	val3 = ASCII_TO_DIGIT(token); */
-	/* 	*integer = (val0 * 1000) + (val1 * 100) + (val2 * 10) + val3; */
-	/* } */
-
-	/* return success; */
-}
-
-int
-main(int argc,
-     char **argv)
+is_jolly_jumper(char *sequence)
 {
 	bool *restrict diff_ptr;
-	char *restrict *restrict from;
+	unsigned int count;
 	int integer;
 	int next;
 	unsigned int diff;
 
-	static bool diff_set[3000] = {
-		[0] = true
-	};
+	count = (unsigned int) READ_INTEGER(sequence);
 
+	bool *const restrict diff_set = calloc(count,
+					       sizeof(bool));
 
-	from = argv + 2;
+	if (diff_set == NULL)
+		exit(1);
 
-	const unsigned int count = argc - 2;
+	diff_set[0] = true;
 
-	char *restrict *const restrict until = from + count;
+	integer = READ_INTEGER(sequence);
 
-	if (!string_to_integer(&integer,
-			       *from)) {
-		WRITE_OUTPUT_LINE("Not jolly");
-		return 0;
-	}
-
-
-	while (1) {
-		++from;
-
-		if (from == until) {
-			WRITE_OUTPUT_LINE("Jolly");
-			return 0;
-		}
-
-		if (!string_to_integer(&next,
-				       *from)) {
-			WRITE_OUTPUT_LINE("Not jolly");
-			return 0;
-		}
+	while (*sequence != '\n') {
+		next = READ_INTEGER(sequence);
 
 		diff = (next > integer)
 		     ? (next - integer)
 		     : (integer - next);
 
 		if (diff >= count) {
-			WRITE_OUTPUT_LINE("Not jolly");
-			return 0;
+			free(diff_set);
+			return false;
 		}
 
 		diff_ptr = &diff_set[diff];
 
 		if (*diff_ptr) {
-			WRITE_OUTPUT_LINE("Not jolly");
-			return 0;
+			free(diff_set);
+			return false;
 		}
 
 		*diff_ptr = true;
 
 		integer = next;
 	}
+
+	free(diff_set);
+	return true;
+}
+
+int
+main(void)
+{
+	char *line;
+	size_t capacity;
+
+	line	 = NULL;
+	capacity = 0;
+
+	while (getline(&line,
+		       &capacity,
+		       stdin) > 0)
+		puts(is_jolly_jumper(line) ? "Jolly" : "Not jolly");
+
+	free(line);
+	return 0;
 }
