c1b37cdcdfc3499d2019c7b2a072197978ac168c
Reid Paape <wrpaape@gmail.com>
2016-11-28 07:32:48 -0600

    red_black_insert

--- red_black/red_black_utils.c
+++ red_black/red_black_utils.c
@@ -14,9 +14,10 @@ node_alloc_pop(void)
 		return node;
 	}
 
-	EXIT_ON_FAILURE("get_key failure: out of space");
+	EXIT_ON_FAILURE("node_alloc_pop failure: out of space");
 	__builtin_unreachable();
 }
+
 
 bool
 red_black_is_member(const struct RedBlackNode *restrict node,
@@ -31,11 +32,52 @@ red_black_is_member(const struct RedBlackNode *restrict node,
 		compare = key_compare(node->key,
 				      key);
 
-		if (compare < 0)
-			node = node->less;
-		if (compare > 0)
-			node = node->more;
-		else
+		if (compare == 0)
 			return true;
+
+		node = (compare < 0)
+		     ? node->left
+		     : node->right;
+	}
+}
+
+static inline void
+red_black_insert_root(struct RedBlackNode *restrict *const restrict tree,
+		      const struct Key *const restrict key)
+{
+	/* static alloc, all pointers NULL */
+	struct RedBlackNode *const restrict node = node_alloc_pop();
+	node->key   = key;
+	node->color = BLACK;
+	*tree = node;
+}
+
+static inline void
+
+void
+red_black_insert(struct RedBlackNode *restrict *const restrict tree,
+		 const struct Key *const restrict key)
+{
+	struct RedBlackNode *restrict parent;
+	struct RedBlackNode *restrict node;
+	int64_t compare;
+
+	parent = *tree;
+
+	if (parent == NULL) {
+		red_black_insert_root(tree,
+				      key);
+		return;
+	}
+
+	compare = key_compare(parent->key,
+			      key);
+
+	if (compare == 0)	/* no duplicates */
+		return;
+
+	if (compare < 0) {
+
+	} else {
 	}
 }
--- red_black/red_black_utils.h
+++ red_black/red_black_utils.h
@@ -18,8 +18,8 @@ struct RedBlackNode {
 	struct Key *key;
 	enum Color color;
 	struct RedBlackNode *parent;
-	struct RedBlackNode *less;
-	struct RedBlackNode *more;
+	struct RedBlackNode *left;
+	struct RedBlackNode *right;
 };
 
 
@@ -28,5 +28,8 @@ bool
 red_black_is_member(const struct RedBlackNode *restrict node,
 		    const struct Key *const restrict key);
 
+void
+red_black_insert(struct RedBlackNode *restrict *const restrict tree,
+		 const struct Key *const restrict key);
 
 #endif /* ifndef RED_BLACK_UTILS_H_ */
